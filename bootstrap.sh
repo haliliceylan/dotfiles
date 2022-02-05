722fef1cb144219c690fbb871c9bb49fb682f4de
Halil <halil>
2022-02-05 14:59:05 +0000

    lets try

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,20 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
+cd ~/dotfiles;
 
 git pull origin main;
 
 function doIt() {
 	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
+		--exclude ".vscodee" \
+		--exclude ".devcontainer" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
-	source ~/.bash_profile;
 }
-
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
+apk add --no-cache zsh
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
+doIt;
 unset doIt;
+zsh
